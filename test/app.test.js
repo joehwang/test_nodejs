38a07581e3706655bd346d19060f3c1e9a6b28e7
joe3_huang <joe3_huang@asus.com>
2023-08-24 14:27:03 +0800

    init

---
+++ test/app.test.js
@@ -0,0 +1,8 @@
+const app = require("../app");
+const request = require("supertest");
+describe("Test the root path", () => {
+  test("It should response the GET method", async () => {
+    const response = await request(app).get("/");
+    expect(response.statusCode).toBe(200);
+  });
+});
